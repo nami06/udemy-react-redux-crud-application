6c720af5cf42a3a4cb6d5d7fda6ed99ee871f386
nami06 <nami.hitoshi06@gmail.com>
2022-12-31 17:24:16 +0900

    repair templates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-function App() {
-  return <div><h1>Hello World</h1></div>;
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <div>Hello World</div>;
+  }
 }
 
 export default App;
